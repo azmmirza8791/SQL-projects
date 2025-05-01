c639e1ef3024a62d1910e289a1d333d57131a075
azmmirza8791 <azeemmirza8791@gmail.com>
2025-05-01 16:16:08 +0530

    Add files via upload

---
+++ pizza_sales_sql_queries/queries/query_1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE order_details(
+order_detail_id int not null,
+order_id int not null,
+pizza_id text not  null,
+quantity int not null,
+primary key (order_detail_id) );
+
+
+
+
+
+
+
+
+
+
